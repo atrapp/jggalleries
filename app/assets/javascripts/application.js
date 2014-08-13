4aef30b47e3939fd72a45925f6ac7fe79e4dd437
atrapp <andrea.trapp@gmx.net>
2014-08-12 21:36:54 -0400

    modified applicaton.css due to heroku push error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,5 @@
 //= require backbone
 //= require jggalleries
 //= require parallax
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
