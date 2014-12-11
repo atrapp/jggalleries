f1986fc160dc3cbdbf995e2013c0ff94f02ccc95
atrapp <andrea.trapp@gmx.net>
2014-12-11 17:03:56 -0500

    added blog MVC and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211215206) do
+ActiveRecord::Schema.define(version: 20141211215915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: true do |t|
+    t.string   "name"
+    t.string   "url_slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "galleries", force: true do |t|
     t.string   "name"
