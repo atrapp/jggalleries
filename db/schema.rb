9b4ac3f7cec5fb1d88a1dcc9c830a54eb00d5e2c
atrapp <andrea.trapp@gmx.net>
2014-12-11 16:44:56 -0500

    added gallery MVC and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813003131) do
+ActiveRecord::Schema.define(version: 20141211213935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleries", force: true do |t|
+    t.string   "name"
+    t.text     "caption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
