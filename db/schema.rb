bc53830ad9730ae0af86b441c5e75f1928660aac
atrapp <andrea.trapp@gmx.net>
2014-12-11 17:13:37 -0500

    added post MVC and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211215915) do
+ActiveRecord::Schema.define(version: 20141211220453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 20141211215915) do
     t.string   "item_id"
     t.text     "caption"
     t.date     "creation_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
