64fa4e941d5805abdb3f31126febe4e66f24dadd
atrapp <andrea.trapp@gmx.net>
2015-01-13 11:41:33 -0500

    reactivated page piling and scrolling via backbone router

--- app/assets/javascripts/routers/jggalleries.js
+++ app/assets/javascripts/routers/jggalleries.js
@@ -10,62 +10,62 @@ Jggalleries.Router = Backbone.Router.extend({
     'studio'    : 'studio',
   },
 
-//   home: function(){
-//       removeActiveClass();
-//       $(".home-menu").addClass("active");
-//       $("html, body").animate({ scrollTop: 0 }, "slow");
-//   },
+  home: function(){
+      removeActiveClass();
+      $(".home-menu").addClass("active");
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+  },
 
-//   galleries: function(){
-//       removeActiveClass();
-//       $(".galleries-menu").addClass("active");
-//       scrollToAnchor('galleries');
-//   },
+  galleries: function(){
+      removeActiveClass();
+      $(".galleries-menu").addClass("active");
+      scrollToAnchor('galleries');
+  },
 
-//   buy: function(){
-//     removeActiveClass();
-//     $(".buy-menu").addClass("active");
-//     scrollToAnchor('buy');
-//   },
+  buy: function(){
+    removeActiveClass();
+    $(".buy-menu").addClass("active");
+    scrollToAnchor('buy');
+  },
 
-//   about: function(){
-//       removeActiveClass();
-//       $(".about-menu").addClass("active");
-//       scrollToAnchor('about');
-//   },
+  about: function(){
+      removeActiveClass();
+      $(".about-menu").addClass("active");
+      scrollToAnchor('about');
+  },
 
-//   blog: function(){
-//       removeActiveClass();
-//       $(".blog-menu").addClass("active");
-//       scrollToAnchor('blog');
-//   },
+  blog: function(){
+      removeActiveClass();
+      $(".blog-menu").addClass("active");
+      scrollToAnchor('blog');
+  },
 
-//   contact: function(){
-//       removeActiveClass();
-//       $(".contact-menu").addClass("active");
-//       scrollToAnchor('contact');
-//   },
+  contact: function(){
+      removeActiveClass();
+      $(".contact-menu").addClass("active");
+      scrollToAnchor('contact');
+  },
 
-//   studio: function(){
-//     removeActiveClass();
-//     $(".studio-menu").addClass("active");
-//     scrollToAnchor('studio');
-//   },
+  studio: function(){
+    removeActiveClass();
+    $(".studio-menu").addClass("active");
+    scrollToAnchor('studio');
+  },
 
 })
 
-// function removeActiveClass(){
-//   $(".galleries-menu").removeClass("active");
-//   $(".buy-menu").removeClass("active");
-//   $(".about-menu").removeClass("active");
-//   $(".blog-menu").removeClass("active");
-//   $(".contact-menu").removeClass("active");
-//   $(".studio-menu").removeClass("active");
-// }
+function removeActiveClass(){
+  $(".galleries-menu").removeClass("active");
+  $(".buy-menu").removeClass("active");
+  $(".about-menu").removeClass("active");
+  $(".blog-menu").removeClass("active");
+  $(".contact-menu").removeClass("active");
+  $(".studio-menu").removeClass("active");
+}
 
-// function scrollToAnchor(anchor){
-//   var aTag = $("#"+ anchor +"-block");
-//   $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-// }
+function scrollToAnchor(anchor){
+  var aTag = $("#"+ anchor +"-block");
+  $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'welcome#home'
+  root 'welcome#index'
 
   resources :users
 
   get '/users/:id/delete_avatar' => 'users#delete_avatar'
 
 end
-
-
-# $ bin/rake routes
-#    Prefix Verb   URI Pattern               Controller#Action
-#      root GET    /                         welcome#index
-#     users GET    /users(.:format)          users#index
-#           POST   /users(.:format)          users#create
-#  new_user GET    /users/new(.:format)      users#new
-# edit_user GET    /users/:id/edit(.:format) users#edit
-#      user GET    /users/:id(.:format)      users#show
-#           PATCH  /users/:id(.:format)      users#update
-#           PUT    /users/:id(.:format)      users#update
-#           DELETE /users/:id(.:format)      users#destroy
