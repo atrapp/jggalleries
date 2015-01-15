e20acb76f9624c08626338faa0bdc3c06530e221
Rebecca50 <rebeccastrong50@gmail.com>
2015-01-15 10:40:12 -0500

    heroku debug by setting app to prodctn > devlpmnt env in config/env/prod.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :recoverable, :trackable, :validatable, :omniauthable, :confirmable, :lockable
-  #devise :database_authenticatable, :registerable, :rememberable, :recoverable, :trackable, :validatable, :omniauthable, :lockable
-  
+  # devise :database_authenticatable, :registerable, :rememberable, :recoverable, :trackable, :validatable, :omniauthable, :confirmable, :lockable
+  devise :database_authenticatable, :registerable, :rememberable, :recoverable, :trackable, :validatable, :omniauthable, :lockable
+
   validates_presence_of :username, on: :create
   validates_uniqueness_of :username
 
