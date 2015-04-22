afaa9b9944397d834269341aefb6ec7b56a2cc51
atrapp <andrea.trapp@gmx.net>
2015-04-22 12:48:43 -0400

    changed image attachment validation in picture model, accepts .jpeg, .jgp, .gif, .png as content-type

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,9 @@
 class Picture < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  # validates_attachment :image, :presence => true,
-  #   :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
-  #   :size => { :less_than => 1.megabytes }
+
+  validates_attachment :image, :presence => true,
+    :content_type => { :content_type => ["image/jpeg", "image/jpg", "image/gif", "image/png"] },
+    :size => { :less_than => 1.megabytes }
 
 end
