160198ae48821fb7802b68d558b133d36a18cbc8
atrapp <andrea.trapp@gmx.net>
2015-04-22 11:24:22 -0400

    added Amazon S3 settings for Paperclip uploads to development.rb and production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV['JGSTUDIOS_DEV_AWS_BUCKET'],
+    :access_key_id => ENV['JGSTUDIOS_AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['JGSTUDIOS_AWS_SECRET_ACCESS_KEY']
   }
 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'jggalleries.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'jg-studios.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true # set 'true' while testing and 'false' when in production
@@ -90,9 +90,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV['JGSTUDIOS_AWS_BUCKET'],
+    :access_key_id => ENV['JGSTUDIOS_AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['JGSTUDIOS_AWS_SECRET_ACCESS_KEY']
   }
 }
 end
